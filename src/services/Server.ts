8af096fb65ef46fda5eeadde9cc4eab17dfc1515
andreas.waldis@stud.hslu.ch <andreas.waldis@stud.hslu.ch>
2017-05-22 18:10:05 +0200

    MultiFiles

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -100,7 +100,7 @@ export class Server {
 
         Data.getData(token, (data: any) => {
             console.log("data ready, sending it to client");
-            const buffer:ResponseData[] = data.map((element)=>{console
+            const buffer:ResponseData[] = data.map((element: any)=>{console
                 return new FileData(element.data, element.path)
             })
             buffer.push(new LastData())
